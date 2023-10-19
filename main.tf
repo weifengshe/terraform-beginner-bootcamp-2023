57c66c3b5ae833f16c480863a9b19f44aeac21e1
weifeng she <weifengshe@gmail.com>
2023-10-19 11:04:35 +0000

    create the root module files

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "weifeng"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.21.0"
-    }
-  }
-  }
-
-
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower = true 
   upper = false 
@@ -40,7 +12,3 @@ resource "aws_s3_bucket" "example" {
   bucket = random_string.bucket_name.result  
 }
 
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-output "random_bucket_name_result" {
-  value = random_string.bucket_name.result
-}
