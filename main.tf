ff3b5fdd2f6833980d2254129e0959d394631f11
weifeng she <weifengshe@gmail.com>
2023-10-19 13:26:32 +0000

    add user_uuid

--- main.tf
+++ main.tf
@@ -10,5 +10,10 @@ resource "aws_s3_bucket" "example" {
   # Bucket naming rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result  
+
+#   tags = {
+#   UserUuid = var.user_uuid
+# }
 }
+
 
