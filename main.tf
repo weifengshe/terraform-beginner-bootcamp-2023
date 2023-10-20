3f72500956182ef5db6483b3b6bbbf33534333f2
weifengshe <weifengshe@gmail.com>
2023-10-19 22:45:41 -0400

    21 restructure root module (#22)
    * Create week0.md
    
    create a week0.md file
    
    * Create .keep
    
    * Update week0.md
    
    Move readme to week0.md
    
    * Create week1.md
    
    Create an empty week1 markdown file
    
    * Update README.md
    
    Add the link in main readme file
    
    * Update week0.md
    
    * Update week1.md
    
    Create the tree structure for the Root module structure
    
    * create the root module files
    
    * add user_uuid
    
    * organize provider tf
    
    * reorganize the root module and write readme.md
    
    * #19 Change structure of root module and add tagging to bucket

--- main.tf
+++ main.tf
@@ -1,31 +1,3 @@
-terraform {
-  cloud {
-    organization = "weifeng"
-    workspaces {
-      name = "terra-house-1"
-    }
-  }
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-     aws = {
-      source = "hashicorp/aws"
-      version = "5.21.0"
-    }
-  }
-  }
-
-
-
-provider "aws" {
-  # Configuration options
-}
-provider "random" {
-  # Configuration options
-}
-
 resource "random_string" "bucket_name" {
   lower = true 
   upper = false 
@@ -38,9 +10,10 @@ resource "aws_s3_bucket" "example" {
   # Bucket naming rules
   # https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
   bucket = random_string.bucket_name.result  
+
+  tags = {
+  UserUuid = var.user_uuid
+}
 }
 
-# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
-output "random_bucket_name_result" {
-  value = random_string.bucket_name.result
-}
+
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "user_uuid" {
+  type        = string
+  description = "User UUID of the user"
+
+  validation {
+    condition     = can(regex("^([0-9a-fA-F]){8}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){4}-([0-9a-fA-F]){12}$", var.user_uuid))
+    error_message = "User UUID must be in the format of a UUID (e.g., 01234567-89ab-cdef-0123-456789abcdef)"
+
+  }
+}
