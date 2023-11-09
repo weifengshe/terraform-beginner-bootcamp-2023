6fdea859372879a8e2093198b64ac95c1f195cee
weifeng she <weifengshe@gmail.com>
2023-11-09 13:32:38 +0000

    Should be able to create and run terraform provider block withoutt errors

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
-output "bucket_name" {
-  description = "Bucket name for our static website hosting"
-  value = module.terrahouse-aws.bucket_name
-}
+# output "bucket_name" {
+#   description = "Bucket name for our static website hosting"
+#   value = module.terrahouse-aws.bucket_name
+# }
 
-output "s3_website_endpoint" {
-  value = module.terrahouse-aws.s3_website_endpoint
-  description = "S3 Static website hosting"
-}
+# output "s3_website_endpoint" {
+#   value = module.terrahouse-aws.s3_website_endpoint
+#   description = "S3 Static website hosting"
+# }
 
-output "cloudfront_url" {
-  value = module.terrahouse-aws.cloudfront_url
-  description = "The CloudFront Distribution Domain Name"
-  }
+# output "cloudfront_url" {
+#   value = module.terrahouse-aws.cloudfront_url
+#   description = "The CloudFront Distribution Domain Name"
+#   }
 
   
