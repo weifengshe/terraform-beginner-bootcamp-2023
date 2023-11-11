a8d6ac9ce84a6bc2466337463aa37d1b2814116f
weifeng she <weifengshe@gmail.com>
2023-11-11 13:28:38 +0000

    # 41 Setup skeleton for resource

--- terraform-provider-terratowns/main.go
+++ terraform-provider-terratowns/main.go
@@ -4,10 +4,11 @@ package main
 
 // fmt is short format, it contains functions for formatted I/O
 import (
-	// "log"
+	"context"
+	"log"
 	"fmt"
-	// "github.com/google/uuid"
-	// "github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/google/uuid"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
@@ -25,11 +26,11 @@ func main() {
 
 }
 
-// type Config struct {
-// 	Endpoint string
-// 	Token string
-// 	UserUuid string  
-// }
+type Config struct {
+	Endpoint string
+	Token string
+	UserUuid string  
+}
 
 // in golang, a titlecase function will get exported 
 
@@ -58,73 +59,94 @@ func Provider() *schema.Provider {
 				Type: schema.TypeString,
 				Required: true,
 				Description: "UUID for configuration",
-				// ValidateFunc: validateUUID, 
+				ValidateFunc: validateUUID, 
 			},
 		},
 	}
-	// P.ConfigureContextFunc = ProviderConfigure(p)
+	p.ConfigureContextFunc = ProviderConfigure(p)
 	return p 
 } 
+// := declare and assign at the same time
+func validateUUID(v interface{}, k string) (ws []string, errors []error){
+	log.Print("validateUUID: start")
+	value := v.(string)
+	if _, err := uuid.Parse(value); err != nil {
+		errors = append(errors, fmt.Errorf("invalid UUID format"))
+	}
+	log.Print("validateUUID:end")
+	return 
+}
 
-// func validateUUID(v interface{}, k string) (ws []string, errors []error){
-// 	log.Print("validateUUID: start")
-// 	value := v.(string)
-// 	if _, err := uuid.Parse(value); err != nil {
-// 		errors = append(errors, fmt.Errorf("invalid UUID format"))
-// 	}
-// 	log.Print("validateUUID:end")
-// 	return 
-// }
 
-// func ProviderConfigure(p *schema.Provider) shcema.ConfigureContextFunc{
-// 	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics ){
-// 		log.Print("providerConfigure:start")
-// 		config := Config{
-// 			Endpoint: d.Get("endpoint").(string),
-// 			Token: d.Get("token").(string),
-// 			UserUuid: d.Get("user_uuid").(string),
-// 		}
-// 		log.Print("providerConfigure:end")
-// 		return &config, nil 
-// 	}
-// }
+func ProviderConfigure(p *schema.Provider) schema.ConfigureContextFunc{
+	return func(ctx context.Context, d *schema.ResourceData) (interface{}, diag.Diagnostics ){
+		log.Print("providerConfigure:start")
+		config := Config{
+			Endpoint: d.Get("endpoint").(string),
+			Token: d.Get("token").(string),
+			UserUuid: d.Get("user_uuid").(string),
+		}
+		log.Print("providerConfigure:end")
+		return &config, nil 
+	}
+}
 
-// func Resource() *schema.Resource{
-// 	log.Print("Resource:start")
-// 	Resource := &schema.Resource{
-// 		CreateContext: resourceHouseCreate,
-// 		ReadContext: resourceHouseRead,
-// 		UpdateContext: resourceHouseUpdate,
-// 		DeleteContext: resourceHouseDelete,
-// 		Schema: map[string]*schema.Schema{
-// 			"name": {
-// 				Type: schema.TypeString,
-// 				Required: true,
-// 				Description: "Name of home",
-// 			},
-// 			"Description": {
-// 				Type: schema.TypeString,
-// 				Required: true,
-// 				Description: "Description of home",
-// 			},
-// 			"Domain_name": {
-// 				Type: schema.TypeString,
-// 				Required: true,
-// 				Description: "Domain name of home, eg. *.cloudfront.net",
-// 			},
-// 			"town": {
-// 				Type: schema.TypeString,
-// 				Required: true,
-// 				Description: "The town to which the home willl belong to",
-// 			},
-// 			"content_version": {
-// 				Type: schema.TypeString,
-// 				Required: true,
-// 				Description: "Then content version of the home",
-// 			},
+func Resource() *schema.Resource{
+	log.Print("Resource:start")
+	resource := &schema.Resource{
+		CreateContext: resourceHouseCreate,
+		ReadContext: resourceHouseRead,
+		UpdateContext: resourceHouseUpdate,
+		DeleteContext: resourceHouseDelete,
+	// 	Schema: map[string]*schema.Schema{
+	// 		"name": {
+	// 			Type: schema.TypeString,
+	// 			Required: true,
+	// 			Description: "Name of home",
+	// 		},
+	// 		"Description": {
+	// 			Type: schema.TypeString,
+	// 			Required: true,
+	// 			Description: "Description of home",
+	// 		},
+	// 		"Domain_name": {
+	// 			Type: schema.TypeString,
+	// 			Required: true,
+	// 			Description: "Domain name of home, eg. *.cloudfront.net",
+	// 		},
+	// 		"town": {
+	// 			Type: schema.TypeString,
+	// 			Required: true,
+	// 			Description: "The town to which the home willl belong to",
+	// 		},
+	// 		"content_version": {
+	// 			Type: schema.TypeString,
+	// 			Required: true,
+	// 			Description: "Then content version of the home",
+	// 		},
 
-// 		},
-// 	}
-// 	log.Print("Resource:start")
-// 	return resource
-// }
+	// 	},
+	}
+	log.Print("Resource:start")
+	return resource
+}
+
+func resourceHouseCreate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics{
+	var diags diag.Diagnostics
+	return diags 
+}
+
+func resourceHouseRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics{
+	var diags diag.Diagnostics
+	return diags 
+}
+
+func resourceHouseUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics{
+	var diags diag.Diagnostics
+	return diags 
+}
+
+func resourceHouseDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics{
+	var diags diag.Diagnostics
+	return diags 
+}
